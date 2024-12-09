245020243a179647304e3f6c9bb0cc5f6045fbad
b11115049 <b11115049@mail.ntust.edu.tw>
2024-12-09 14:25:36 +0800

    lab2

--- ioctl/mydevice/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/nachos/hw5/OS_HW5/ioctl/mydevice/modules.order := {   echo /home/nachos/hw5/OS_HW5/ioctl/mydevice/myioctl_driver.o; :; } > /home/nachos/hw5/OS_HW5/ioctl/mydevice/modules.order
--- ioctl/mydevice/modules.order
+++
@@ -1,1 +0,0 @@
-/home/nachos/hw5/OS_HW5/ioctl/mydevice/myioctl_driver.o
